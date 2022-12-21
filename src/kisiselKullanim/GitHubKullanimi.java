43b94b1215f7b5c0d896b23c9c1480670983528d
saidozkan2020 <said_ozkan@hotmail.com>
2022-12-21 22:26:38 +0100

    deneme4

--- src/kisiselKullanim/GitHubKullanimi.java
+++ src/kisiselKullanim/GitHubKullanimi.java
@@ -1,7 +1,7 @@
 package kisiselKullanim;
 
 public class GitHubKullanimi {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Hello world!");
 
         System.out.println("Zafer abi");
@@ -12,6 +12,6 @@ public class GitHubKullanimi {
 yapistirdim ama görünmüyor
 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
  */
-        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
+
     }
-}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
+}
