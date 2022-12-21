f2ab8303b15adbcda6b4fb45d659b8846d7fea4a
saidozkan2020 <said_ozkan@hotmail.com>
2022-12-21 15:30:14 +0100

    firstSaid

--- src/kisiselKullanim/GitHubKullanimi.java
+++ src/kisiselKullanim/GitHubKullanimi.java
@@ -3,5 +3,8 @@ package kisiselKullanim;
 public class GitHubKullanimi {
     public static void main(String[] args){
         System.out.println("Hello world!");
+
+        System.out.println("Zafer abi");
+
     }
 }
